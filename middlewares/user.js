6a5d86ea9d3c2c773e139caab78c14b526b8c8d4
Cecilia1096 <angiefd1096@gmail.com>
2020-12-13 19:31:28 -0600

    endpoints forgot and reset password

--- middlewares/user.js
+++ middlewares/user.js
@@ -77,6 +77,78 @@ router.post("/login", async (req, res) => {
         res.status(500).json({ error: error.message })
       }
     });
+
+router.post("/tokenisvalid", async (req, res) => {
+      try {
+        const token = req.header("x-auth-token");
+        if (!token) return res.json(false);
+    
+        const verified = jwt.verify(token, process.env.JWT_SECRET);
+        if (!verified) return res.json(false);
+    
+        const User = await user.findById(verified.id);
+        if (!User) return res.json(false);
+    
+        return res.json(true);
+      } catch (err) {
+        res.status(500).json({ error: err.message });
+      }
+    });
+    
+router.post('/forgot-password', (req,res)=>{
+      crypto.randomBytes(32,(err,buffer)=> {
+          if(err){
+              console.log(err)
+          }
+          const token = buffer.toString("hex")
+          user.findOne({email:req.body.email})
+          .then(User => {
+             if(!User){
+                return res.status(422).json({error:"There is no user with this email"})
+             }
+           User.resetToken = token
+           User.expireToken = Date.now() + 60000
+           User.save().then((result)=>{
+               const data = {
+                 to:User.email,
+                 from:"no-replay@clickit.com",
+                 subject:"Restablecimiento de contraseña",
+                 html:`
+                 <p>Solicitaste restablecer tu contraseña,</p>
+                 <h5>click here:<a href="http://localhost:3000/reset-password/${token}">link</a>to reset password</h5>
+                 `
+               }
+               mg.messages().send(data, function (error,body){
+                  if(error){
+                    return res.json({error:"Link error when resetting password"})
+                  }
+               })
+               res.json({message:"Check your email"})
+           })
+          })
+      })
+ })
+ 
+router.post('/reset-password',(req,res) => {
+   const newPassword = req.body.password
+   const sentToken = req.body.token
+   user.findOne({resetToken:sentToken,expireToken:{$gt:Date.now()}})
+   .then(User=>{
+       if(!User){
+           return res.status(422).json({error:"Please try again, session expired"})
+       }
+       bcrypt.hash(newPassword,12).then(hashedpassword=>{
+          User.password = hashedpassword
+          User.resetToken = undefined
+          User.expireToken = undefined
+          User.save().then((saveduser)=>{
+              res.json({message:"Password updated successfully"})
+          })
+       })
+   }).catch(err=>{
+       console.log(err)
+   })
+ })
   
   
 module.exports = router;
